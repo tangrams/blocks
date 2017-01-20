799e06c658e802c88d4b890b056dbe56f1c653dd
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2017-01-20 11:07:20 -0500

    only benchmark on th pi

--- setup.py
+++ setup.py
@@ -146,8 +146,8 @@ if len(sys.argv) > 1:
         benchmarks()
 else:
     standaloneBlocks()
-    # if isRPi():
-        # benchmarks()
-    benchmarks()
+    if isRPi():
+        benchmarks()
+    # benchmarks()
     document()
     
